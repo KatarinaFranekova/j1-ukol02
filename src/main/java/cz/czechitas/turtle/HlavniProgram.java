fc84ae2256c7997f5ff8d5b2f1bec8da3d4bed8c
KatarinaFranekova <katarinafranekova22@gmail.com>
2023-02-23 14:24:05 +0100

    zmrzka a masinka

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -7,12 +7,72 @@ import java.awt.*;
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
-    Color hlava = new Color(51-204-255);
-    Color spodnaCast = new Color(0-0-204);
-    Color telo = new Color(0-0-255);
+    Color hlava = new Color(51,204,255);
+    Color spodnaCast = new Color(0,0,204);
+    Color telo = new Color(0,0,255);
     public void start() {
 
-        zofka.setLocation(200.0, 200.0);
+
+        // MASINKA
+
+        nakresliObdlznik(100,200, Color.orange);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(100);
+        //  zofka.turnRight(180);
+        nakresliObdlznik2(300,100, Color.green);
+
+        zofka.penUp();
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnRight(90);
+        zofka.move(25);
+        // zofka.turnRight(180);
+
+        nakresliKruh(40, Color.pink);
+
+        zofka.turnRight(106);
+        //zofka.turnLeft(144);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.turnLeft(90);
+        zofka.move(20);
+        nakresliKruh(20, Color.pink);
+
+        zofka.turnRight(20);
+        zofka.penUp();
+        zofka.move(130);
+        nakresliKruh(20, Color.pink);
+
+        zofka.penUp();
+        zofka.move(120);
+        zofka.turnLeft(162);
+
+        double stranaTrojuholnika = 100;
+        double preponaT = Math.sqrt(2*Math.pow(stranaTrojuholnika, 2));
+        zofka.penDown();
+        zofka.setPenColor(Color.black);
+        zofka.move(stranaTrojuholnika);
+        zofka.turnRight(90);
+        zofka.move(stranaTrojuholnika);
+        zofka.turnRight(135);
+        zofka.move(preponaT);
+        //koniec masinky
+
+        //presun dolava
+
+        zofka.turnLeft(45);
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(700);
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.penDown();
+
+
+        // ZMRZLINA
+
         nakresli10uholnik(50, Color.pink);
 
 
@@ -33,43 +93,40 @@ public class HlavniProgram {
         zofka.turnLeft(107);
         zofka.penUp();
         zofka.move(400);
-        zofka.turnLeft(90);hjhjhjhj
+        zofka.turnLeft(90);
+        zofka.move(150);
 
+        //koniec zmrzliny
 
-        nakresli10uholnik(130, Color.blue);
-        nakresli10uholnik(100, Color.red);
-        nakresli10uholnik(70, Color.orange);
+        nakresli10uholnik(100, spodnaCast);
+        nakresli10uholnik(70, telo);
+        nakresli10uholnik(50, hlava);
 
-       nakresli10uholnik(30, Color.pink);
-
-
-
-
-/*        nakresliStvorec(50, Color.yellow);
-
-
-        nakresliObdlznik(50, 200, Color.blue);
-
-        zofka.move(100);
-
-        double stranaT = 100;
-        double prepona = Math.sqrt(2*Math.pow(stranaT, 2));
-        nakresliPravRovnoTrojuhol(100, prepona, Color.black);*/
 
     }
 
+    public void nakresliTrojuholnik (double velkostStranyT, Color farbaTrojuholnika) {
+        zofka.setPenColor(farbaTrojuholnika);
+        zofka.move(velkostStranyT);
+        zofka.turnLeft(120.0);
+        zofka.move(velkostStranyT);
+        zofka.turnLeft(120.0);
+        zofka.move(velkostStranyT);
+        zofka.turnLeft(120.0);
+    }
+    public void nakresliObdlznik2 (double stranaX, double stranaY, Color farbaObdl) {
+        zofka.setPenColor(farbaObdl);
+        zofka.penDown();
+        zofka.turnRight(90);
+        zofka.move(stranaX);
+        zofka.turnLeft(90);
+        zofka.move(stranaY);
+        zofka.turnLeft(90);
+        zofka.move(stranaX);
+    }
 
-    /*public void nakresliSnehuliaka (double velkostStrany, Color farba) {
-        zofka.setPenColor(farba);
-        for (int x = 0; x <14; x++) {
-            zofka.turnLeft(30);
-            zofka.move(30);
-
-    }*/
-
-
-   /* public void nakresliPravRovnoTrojuhol (double stranaT, double prepona, Color farbaT) {
-       // double prepona = Math.sqrt(2*Math.pow(stranaT, 2));
+    public void nakresliPravRovnoTrojuhol (double stranaT, double prepona, Color farbaT) {
+        // double prepona = Math.sqrt(2*Math.pow(stranaT, 2));
         zofka.setPenColor(farbaT);
         zofka.penDown();
         zofka.turnRight(45);
@@ -80,6 +137,14 @@ public class HlavniProgram {
         zofka.move(prepona);
     }
 
+    public void nakresliStvorec (double stranaStvorca, Color farbaStvorca) {
+        zofka.setPenColor(farbaStvorca);
+        for (int s = 0; s < 4; s++) {
+            zofka.penDown();
+            zofka.turnRight(90);
+            zofka.move(stranaStvorca);
+        }
+    }
     public void nakresliObdlznik (double stranaA, double stranaB, Color farbaObdlznika) {
         zofka.setPenColor(farbaObdlznika);
         for (int o = 0; o < 2; o++) {
@@ -89,26 +154,18 @@ public class HlavniProgram {
             zofka.turnRight(90);
             zofka.move(stranaB);
         }
-
-
-
-
     }
-
-
-
-
-
-    public void nakresliStvorec (double stranaStvorca, Color farbaStvorca) {
-        zofka.setPenColor(farbaStvorca);
-        for (int s = 0; s < 4; s++) {
-            zofka.penDown();
-            zofka.turnRight(90);
-            zofka.move(stranaStvorca);
+    public void nakresliKruh(double dlzkaStrany, Color farbaKruhu) {
+        zofka.setPenColor(farbaKruhu);
+        zofka.penDown();
+        for (int z = 0; z < 12; z++) {
+            zofka.turnRight(36);
+            zofka.move(dlzkaStrany);
         }
-    }*/
+    }
     public void nakresli10uholnik(double dlzkaStrany, Color farbaCiar)     {
-        zofka.setPenColor(farbaCiar);  zofka.penDown();
+        zofka.setPenColor(farbaCiar);
+        zofka.penDown();
         for (int i = 0; i <12; i++) {
             zofka.turnLeft(36);
             zofka.move(dlzkaStrany);
